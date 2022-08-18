d651be76c3ec17092860af2706f031f22746b69d
Pradip Gharti Magar <thapadeep234@gmail.com>
2022-08-18 13:00:19 +0545

    updated

--- app.py
+++ app.py
@@ -1,11 +1,230 @@
-from flask import Flask
+from flask import Flask, request
+import sys
+from wsgiref import simple_server
+import pip
+from bike.util.util import read_yaml_file, write_yaml_file
+from matplotlib.style import context
+from bike.logger import logging
+from bike.exception import bikeException
+import os, sys
+import json
+from bike.config.configuration import Configuartion
+from bike.constant import CONFIG_DIR, get_current_time_stamp
+from bike.pipeline.pipeline import Pipeline
+from bike.entity.bike_predictor import HousingPredictor, HousingData
+from flask import send_file, abort, render_template
+
+
+ROOT_DIR = os.getcwd()
+LOG_FOLDER_NAME = "logs"
+PIPELINE_FOLDER_NAME = "bike"
+SAVED_MODELS_DIR_NAME = "saved_models"
+MODEL_CONFIG_FILE_PATH = os.path.join(ROOT_DIR, CONFIG_DIR, "model.yaml")
+LOG_DIR = os.path.join(ROOT_DIR, LOG_FOLDER_NAME)
+PIPELINE_DIR = os.path.join(ROOT_DIR, PIPELINE_FOLDER_NAME)
+MODEL_DIR = os.path.join(ROOT_DIR, SAVED_MODELS_DIR_NAME)
+
+
+from bike.logger import get_log_dataframe
+
+BIKE_DATA_KEY = "bike_data"
+COUNT_VALUE_KEY = "cnt"
 
 app = Flask(__name__)
 
-@app.route("/",methods = ['GET','POST'])
-def index():
-    return "CI/CD pipeline established"
 
-if __name__=="__main__":
-    app.run(debug=True)
-    
+@app.route('/artifact', defaults={'req_path': 'bike'})
+@app.route('/artifact/<path:req_path>')
+def render_artifact_dir(req_path):
+    os.makedirs("bike", exist_ok=True)
+    # Joining the base and the requested path
+    print(f"req_path: {req_path}")
+    abs_path = os.path.join(req_path)
+    print(abs_path)
+    # Return 404 if path doesn't exist
+    if not os.path.exists(abs_path):
+        return abort(404)
+
+    # Check if path is a file and serve
+    if os.path.isfile(abs_path):
+        if ".html" in abs_path:
+            with open(abs_path, "r", encoding="utf-8") as file:
+                content = ''
+                for line in file.readlines():
+                    content = f"{content}{line}"
+                return content
+        return send_file(abs_path)
+
+    # Show directory contents
+    files = {os.path.join(abs_path, file_name): file_name for file_name in os.listdir(abs_path) if
+             "artifact" in os.path.join(abs_path, file_name)}
+
+    result = {
+        "files": files,
+        "parent_folder": os.path.dirname(abs_path),
+        "parent_label": abs_path
+    }
+    return render_template('files.html', result=result)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    try:
+        return render_template('index.html')
+    except Exception as e:
+        return str(e)
+
+
+@app.route('/view_experiment_hist', methods=['GET', 'POST'])
+def view_experiment_history():
+    experiment_df = Pipeline.get_experiments_status()
+    context = {
+        "experiment": experiment_df.to_html(classes='table table-striped col-12')
+    }
+    return render_template('experiment_history.html', context=context)
+
+
+@app.route('/train', methods=['GET', 'POST'])
+def train():
+    message = ""
+    pipeline = Pipeline(config=Configuartion(current_time_stamp=get_current_time_stamp()))
+    if not Pipeline.experiment.running_status:
+        message = "Training started."
+        pipeline.start()
+    else:
+        message = "Training is already in progress."
+    context = {
+        "experiment": pipeline.get_experiments_status().to_html(classes='table table-striped col-12'),
+        "message": message
+    }
+    return render_template('train.html', context=context)
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+    context = {
+        BIKE_DATA_KEY: None,
+        COUNT_VALUE_KEY: None
+    }
+
+    if request.method == 'POST':
+        season = (request.form['season'])
+        yr = (request.form['yr'])
+        mnth = (request.form['mnth'])
+        hr = (request.form['hr'])
+        holiday = (request.form['holiday'])
+        weekday = (request.form['weekday'])
+        workingday = (request.form['workingday'])
+        weathersit = (request.form['weathersit'])
+        temp = request.form['temp']
+        atemp = (request.form['atemp'])
+        hum = (request.form['hum'])
+        windspeed = (request.form['windspeed'])
+
+        housing_data = HousingData(season=season,
+                                   yr=yr,
+                                   mnth=mnth,
+                                   hr=hr,
+                                   holiday=holiday,
+                                   weekday=weekday,
+                                   workingday=workingday,
+                                   weathersit=weathersit,
+                                   temp=temp,
+                                   atemp=atemp,
+                                   hum=hum,
+                                   windspeed=windspeed,
+                                   )
+        housing_df = housing_data.get_housing_input_data_frame()
+        housing_predictor = HousingPredictor(model_dir=MODEL_DIR)
+        cnt = housing_predictor.predict(X=housing_df)
+        context = {
+            BIKE_DATA_KEY: housing_data.get_housing_data_as_dict(),
+            COUNT_VALUE_KEY: cnt,
+        }
+        return render_template('predict.html', context=context)
+    return render_template("predict.html", context=context)
+
+
+@app.route('/saved_models', defaults={'req_path': 'saved_models'})
+@app.route('/saved_models/<path:req_path>')
+def saved_models_dir(req_path):
+    os.makedirs("saved_models", exist_ok=True)
+    # Joining the base and the requested path
+    print(f"req_path: {req_path}")
+    abs_path = os.path.join(req_path)
+    print(abs_path)
+    # Return 404 if path doesn't exist
+    if not os.path.exists(abs_path):
+        return abort(404)
+
+    # Check if path is a file and serve
+    if os.path.isfile(abs_path):
+        return send_file(abs_path)
+
+    # Show directory contents
+    files = {os.path.join(abs_path, file): file for file in os.listdir(abs_path)}
+
+    result = {
+        "files": files,
+        "parent_folder": os.path.dirname(abs_path),
+        "parent_label": abs_path
+    }
+    return render_template('saved_models_files.html', result=result)
+
+
+
+
+@app.route("/update_model_config", methods=['GET', 'POST'])
+def update_model_config():
+    try:
+        if request.method == 'POST':
+            model_config = request.form['new_model_config']
+            model_config = model_config.replace("'", '"')
+            print(model_config)
+            model_config = json.loads(model_config)
+
+            write_yaml_file(file_path=MODEL_CONFIG_FILE_PATH, data=model_config)
+
+        model_config = read_yaml_file(file_path=MODEL_CONFIG_FILE_PATH)
+        return render_template('update_model.html', result={"model_config": model_config})
+
+    except  Exception as e:
+        logging.exception(e)
+        return str(e)
+
+
+@app.route(f'/logs', defaults={'req_path': f'{LOG_FOLDER_NAME}'})
+@app.route(f'/{LOG_FOLDER_NAME}/<path:req_path>')
+def render_log_dir(req_path):
+    os.makedirs(LOG_FOLDER_NAME, exist_ok=True)
+    # Joining the base and the requested path
+    logging.info(f"req_path: {req_path}")
+    abs_path = os.path.join(req_path)
+    print(abs_path)
+    # Return 404 if path doesn't exist
+    if not os.path.exists(abs_path):
+        return abort(404)
+
+    # Check if path is a file and serve
+    if os.path.isfile(abs_path):
+        log_df = get_log_dataframe(abs_path)
+        context = {"log": log_df.to_html(classes="table-striped", index=False)}
+        return render_template('log.html', context=context)
+
+    # Show directory contents
+    files = {os.path.join(abs_path, file): file for file in os.listdir(abs_path)}
+
+    result = {
+        "files": files,
+        "parent_folder": os.path.dirname(abs_path),
+        "parent_label": abs_path
+    }
+    return render_template('log_files.html', result=result)
+
+port = int(os.getenv("PORT", 5000))
+if __name__ == "__main__":
+    host = '0.0.0.0'
+    # port = 5000
+    httpd = simple_server.make_server(host, port, app)
+    # print("Serving on %s %d" % (host, port))
+    httpd.serve_forever()
